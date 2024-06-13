89b9abbdc2d4b319b7a23bd07b0066c59e2bb92e
ahmdsydysf <a.sayed.xc@gmail.com>
2024-06-13 17:46:44 +0300

    session--02

--- main.php
+++ main.php
@@ -1,11 +1,141 @@
+<?php
+
+// $myname = "ahmed20";
+// $var2 = "55";
+
+// echo "myname is $myname and my age is $var2";
+// echo 'myname is' . $myname .' and my age is ' . $var2;
+
+//cs
+
+// $myName = 'ahmed sayed';
+// $myNamE = 'ahmed sayed2';
+
+
+// php   loosely typed lang
+
+// assigned by value   php by default
+
+// $x = 50;
+
+// $y = &$x;
+
+// $x = 200;
+
+// echo $y;
+
+
+
+// comments type  //    /*   */    DocBlock
+/**
+ *
+ *
+ * dfsf]
+ * sdf
+ * sdf
+ * sdf
+ *
+ */
+
+
+// echo $firstName;
+// echo '$firstName';
+// echo "$firstName";
+// echo 'my name is $firstName $lastName';
+// echo "my name is $firstName $lastName";
+// echo "my name is {$firstName} {$lastName}";
+// echo 'my name is {$firstName} {$lastName}';
+
+
+//  constant   defined and have value on same line  global  predefined const  ,  magic const
+// $name ;
+// $name = 'mohamed';
+// $name = 'ahmed';
+// echo $name ;
+
+// define("DATABASENAME", 'AhmedStore2');
+
+// $firstName = "ahmed2";
+// $lastName = "sayed";
+// define("DATABASENAME", $firstName);
+
+
+// function test()
+// {
+//     global $firstName;
+//     echo $firstName;
+// }
+
+// test();
+
+// echo PHP_OS_FAMILY;
+// echo PHP_VERSION;
+
+// echo __LINE__;
+// echo __DIR__;
+
+// variable variables
+
+// $x = 'name';
+// $$x = "6565";
+
+// echo "{$$x}";
+
+// //  soft Debug
+// var_dump($$x);
+// // print_r();
+
+// DataTypes
+
+
+// scalar Types   bool (true , false)  int (100)  float(10.0556)  string (''"")
+// computed Types   array  object   callable   iterable
+// special Types   resource    null
+
+
+// $isConnected = true ;
+// $isOffline = false ;
+
+// var_dump($isConnected);
+// var_dump($isOffline);
+// // print_r($isConnected);
+// // print_r($isOffline);
+
+echo true ;
+echo false ;// empty string
+
+$test = null;
+
+// falsy values   false , 0 -0  0.0  -0.0  null  []  '0'   ''
+
+if($test == true) {
+    echo "this is truthy value";
+} else {
+    echo "this is falsy value";
+}
+
+echo PHP_INT_MIN;
+echo PHP_INT_MIN;
+echo PHP_INT_MIN;
+
+echo PHP_INT_MAX;
+echo PHP_INT_MIN;
+echo PHP_INT_MIN;
+echo PHP_INT_MIN;
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
 </head>
 <body>
-  <h1>Ahmed Sayed</h1>
+  <p>Lorem ipsum dolor sit amet.</p>
+
 </body>
 </html>
