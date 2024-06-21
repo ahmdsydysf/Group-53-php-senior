2e10cd24a59f5338d39b14e44707731d5c8bf96d
ahmdsydysf <a.sayed.xc@gmail.com>
2024-06-21 21:54:15 +0300

    session 4 update

--- main.php
+++ main.php
@@ -219,11 +219,11 @@
 
 // $x['php'];
 
-$degrees = [50,256,4,7,258,4,8];
+// $degrees = [50,256,4,7,258,4,8];
 
-$degreesOfSTD = ['ahmed' => 50,  6585 => 'y', 'mohamed' => 256,'sayed' => 4,'gamal' => 7 ];
+// $degreesOfSTD = ['ahmed' => 50,  6585 => 'y', 'mohamed' => 256,'sayed' => 4,'gamal' => 7 ];
 
-echo $degreesOfSTD['mohamed'];
+// echo $degreesOfSTD['mohamed'];
 
 
 
@@ -332,24 +332,206 @@ echo $degreesOfSTD['mohamed'];
 
 
 
-var_dump(580 <=> 580);
+// var_dump(580 <=> 580);
 
 //  <     =    >
 //  -1    0   1
 
+
+
+//  ternary operator
+
+
+// if($is_user_logged_in == true ) {
+//     echo "<button> Logout </button>";
+// } else {
+//     echo "<button> Login </button>";
+// }
+
+// $is_user_logged_in = true;
+// $role_of_user = 'normal user';
+
+// $status = $is_user_logged_in == true ? '<button> Logout </button>' : '<button> Login </button>';
+
+
+
+
+
+// $status = $is_user_logged_in == true ?
+//       ($role_of_user != 'admin' ?
+//               '<button>logout</button>' :
+//               '<button>logout</button><button>dashboard</button>')
+//       : '<button> Login </button>';
+
+// // null coalescing ??
+// // falsy values   false , 0 -0  0.0  -0.0  null  []  '0'   ''        ?:
+
+// $path = 0;
+// $url = $path ?: '/';
+
+// echo $url ;
+
+// echo $status
+
+// $x = 5;
+// echo $x++; // post increment  first return $x  after that increment 1
+// echo $x;
+
+// $y = 10;
+// echo ++$y; // pre increment
+// echo $y;
+
+// $j = null;
+// echo ++$j;
+// echo $j;
+
+// $t = 'karin';
+// echo ++$t;
+
+// logical operator  ( and  &   && ) al must be true ( or |  || ) one true  ( xor ) one true but not both
+
+// $book = true;
+// $laptop = true;
+
+// ($book || $laptop)
+
+// $x = true;
+// $y = false;
+// $z = true;
+
+// var_dump( $x && !$y || $z && false);
+
+// 1  0000 0001
+
+// (0 )
+//  0000 0101  xor  0000 0011
+//  0000 0110
+// var_dump(~6 ^ 3)
+// $x = 6 ;//0000 0011
+// $y = 1 ;
+// // >>  shift right
+// var_dump($x >> $y) ;//3
+
+// $x = 6 ;//0011 0000
+// $y = 3 ;
+// var_dump($x << $y);
+
+// $x = ['aa' => 'a' ,'bb' => 'b' ,'cc' => 'c'];
+// $y = ['a' => 'd' , 'b' => 'e' ,'c' => 'f', 'gg' => 'g', 'hh' => 'h'];
+
+// var_dump($x + $y);
+
+// $x = [ 0 => '1' , 'b' => 2];
+// $y = ['0' => '1' , 'b' => 2];
+
+// var_dump($x <> $y);
+// $x = ['a' => '1' , 'b' => 2];
+// $y = ['a' => 1 , 'b' => '2'];
+
+// var_dump($x === $y);
+$databaseName = 'Souq' ;
+// if($databaseName == 'Souq' && 'ahmed' === 'ahmed' || true == false) {
+//     echo "ahmed";
+// }
+
+// if($databaseName == 'Souq' && 'ahmed' === 'ahmed1' || true == false) {
+//     echo "from true";
+// } else {
+//     echo 'from else';
+// }
+
+// if(){
+
+// }elseif(){
+
+// }else if(){
+
+// }
+
+$finalResult = 89 ;
+
+// if($finalResult >= 90) {
+//     echo '<h1> A </h1>';
+// } elseif($finalResult < 90 && $finalResult >= 80) {
+//     echo '<h1> B </h1>';
+// } elseif($finalResult < 80 && $finalResult >= 50) {
+//     echo '<h1> C </h1>';
+// } else {
+//     echo '<h1> F </h1>';
+// }
+
+// <?php if($finalResult >= 90):
+//   <h1 class='' id=''> Is A+ </h1>
+// <?php elseif($finalResult < 90 && $finalResult >= 80)
+//   <h1> Is B+ </h1>
+// <?php elseif($finalResult < 80 && $finalResult >= 50):
+//   <h1> Is C+ </h1>
+// <?php else :
+//   <h1> Is F </h1>
+// <?php endif
+
+
+
+// while($x < 10) {
+//     echo '<h1> my  number is ' . $x . '</h1>';
+//     $x++;
+// }
+
+
+// <?php while($x < 10):
+
+//   <div class="card" style="width: 18rem;">
+//   <img src="..." class="card-img-top" alt="...">
+//   <div class="card-body">
+//     <h5 class="card-title">Card title <?= $x </h5>
+//     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//     <a href="#" class="btn btn-primary">Go somewhere</a>
+//   </div>
+// </div>
+// <?php $x++
+// <?php endwhile
+
+// $x = 1 ;
+// $y = 5 ;
+
+
+// for($i = 0 ; $i < 5 ; $i++) {
+//     echo "<h1> num is $i </h1>";
+// }
+
+
+// for(variable; condition ; inc or dec){
+
+// }
+
+// do {
+//     echo 'num is' . $x . ' , ';
+//     $x++;
+// } while($x > 10);
+
+
+// while($x !== $y) {
+//     if($x == 3) {
+//       continue;
+//         $x++;
+//     }
+//     echo $x;
+//     $x++;
+// }
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-  <p>Lorem ipsum dolor sit amet.</p>
-
+<?php for($i = 0 ; $i < 5 ; $i++): ?>
+  <h1>  </h1>
+<?php endfor ?>
 </body>
 </html>
