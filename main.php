e6972a410fdf0ccf1bcb9f28f46a7a571d40e555
ahmdsydysf <a.sayed.xc@gmail.com>
2024-06-25 20:46:35 +0300

    v2

--- main.php
+++ main.php
@@ -530,8 +530,11 @@ $finalResult = 89 ;
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-<?php for($i = 0 ; $i < 5 ; $i++): ?>
-  <h1>  </h1>
+
+<?php for($i = 0 , print $i ; $i <= 5 ; $i++): ?>
+
+  <h1> ahmed sayed <?= $i ?> </h1>
+
 <?php endfor ?>
 </body>
 </html>
